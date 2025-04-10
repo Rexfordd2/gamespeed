405c0e9b82a029148b161b8c9bb879e3b3fb3a27
Rexfordd2 <64678115+Rexfordd2@users.noreply.github.com>
2025-04-10 10:05:24 -0700

    Fix: simplify vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import { resolve } from 'path'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // This ensures assets are loaded correctly
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
-    minify: 'terser',
-    target: 'esnext',
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
-      output: {
-        manualChunks: {
-          'react-vendor': ['react', 'react-dom'],
-          'framer-motion': ['framer-motion']
-        }
-      }
-    }
-  },
-  esbuild: {
-    jsxInject: `import React from 'react'`,
-  },
 }) 
